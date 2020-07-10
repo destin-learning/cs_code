0d2c408c116841b78d6ea2ce867a894ed945e6dd
erfrick <47677245+erfrick@users.noreply.github.com>
2020-07-09 22:18:21 -0400

    Create Program.cs
    Lab for Chapter 15

---
+++ ch15/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using static System.Console;
+
+namespace ch15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
