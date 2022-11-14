67ae260ab7a8213664e57b02b458125d38d467bb
erfrick <47677245+erfrick@users.noreply.github.com>
2022-11-14 16:08:44 -0500

    Update Program.cs

--- ch4/Program.cs
+++ ch4/Program.cs
@@ -11,13 +11,13 @@ namespace ch4
             String myName;
  
             // 6.2 prompt to enter in your name
-            System.Console.WriteLine("Please Enter Your Name:");
+            Console.WriteLine("Please Enter Your Name:");
  
             // 6.3 now read in your name 
             myName = Console.ReadLine();
  
             // 6.4 add in + myName to print out your name
-            System.Console.WriteLine("Hello World My Name is:"+myName);
+            Console.WriteLine("Hello World My Name is:"+myName);
 
         }
     }
